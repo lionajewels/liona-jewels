56390e2b0b912bee495ef1fa87ee14543f2fb843
Laura Maldonado <lauramaldonado@MacBook-Air-5.local>
2026-07-14 13:55:58 +0200

    Otro puerto

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 ========================= */
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Puerto 587 usa STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
